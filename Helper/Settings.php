98a6f7546761b0023eac1254d0253bd997993f1e
David Hein <david.hein@wika.com>
2025-03-24 13:28:36 +0100

    Allow admin backend settings for each store

--- Helper/Settings.php
+++ Helper/Settings.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace WikaGroup\AzureB2cSSO\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
 
 class Settings extends AbstractHelper
 {
@@ -15,61 +16,61 @@ class Settings extends AbstractHelper
 
     public function isSsoEnabled(): bool
     {
-        return (bool)$this->scopeConfig->getValue('azure_b2c/general/enable_sso');
+        return (bool)$this->scopeConfig->getValue('azure_b2c/general/enable_sso', ScopeInterface::SCOPE_STORES);
     }
 
     public function showButton(): bool
     {
-        return (bool)$this->scopeConfig->getValue('azure_b2c/general/show_button');
+        return (bool)$this->scopeConfig->getValue('azure_b2c/general/show_button', ScopeInterface::SCOPE_STORES);
     }
 
     public function createMagentoCustomer(): bool
     {
-        return (bool)$this->scopeConfig->getValue('azure_b2c/general/create_mage_customer');
+        return (bool)$this->scopeConfig->getValue('azure_b2c/general/create_mage_customer', ScopeInterface::SCOPE_STORES);
     }
 
     public function useCustomCss(): bool
     {
-        return (bool)$this->scopeConfig->getValue('azure_b2c/general/use_custom_css');
+        return (bool)$this->scopeConfig->getValue('azure_b2c/general/use_custom_css', ScopeInterface::SCOPE_STORES);
     }
 
     public function getCustomContainerCss(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/general/custom_container_class');
+        return (string)$this->scopeConfig->getValue('azure_b2c/general/custom_container_class', ScopeInterface::SCOPE_STORES);
     }
 
     public function getCustomButtonCss(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/general/custom_button_class');
+        return (string)$this->scopeConfig->getValue('azure_b2c/general/custom_button_class', ScopeInterface::SCOPE_STORES);
     }
 
     public function logoutFromAzure(): bool
     {
-        return (bool)$this->scopeConfig->getValue('azure_b2c/general/log_out_from_azure');
+        return (bool)$this->scopeConfig->getValue('azure_b2c/general/log_out_from_azure', ScopeInterface::SCOPE_STORES);
     }
 
     public function getClientId(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/connection/client_id');
+        return (string)$this->scopeConfig->getValue('azure_b2c/connection/client_id', ScopeInterface::SCOPE_STORES);
     }
 
     public function getClientSecret(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/connection/client_secret');
+        return (string)$this->scopeConfig->getValue('azure_b2c/connection/client_secret', ScopeInterface::SCOPE_STORES);
     }
 
     public function getBaseUrl(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/connection/base_url');
+        return (string)$this->scopeConfig->getValue('azure_b2c/connection/base_url', ScopeInterface::SCOPE_STORES);
     }
 
     public function getDefaultAlgorithm(): string
     {
-        return (string)$this->scopeConfig->getValue('azure_b2c/connection/default_algorithm');
+        return (string)$this->scopeConfig->getValue('azure_b2c/connection/default_algorithm', ScopeInterface::SCOPE_STORES);
     }
 
     public function getLeewayTime(): int
     {
-        return (int)$this->scopeConfig->getValue('azure_b2c/connection/leeway');
+        return (int)$this->scopeConfig->getValue('azure_b2c/connection/leeway', ScopeInterface::SCOPE_STORES);
     }
 }
