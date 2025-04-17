cde61255cf5b7f0281e72e6392e62de6bda2269b
David Hein <david.hein@wika.com>
2025-04-17 08:26:15 +0200

    Fixed usage of wrong group

--- Model/Config/Source/Group.php
+++ Model/Config/Source/Group.php
@@ -7,16 +7,16 @@ class Group implements \Magento\Framework\Data\OptionSourceInterface
     private ?array $options = null;
 
     public function __construct(
-        private \Magento\Store\Model\ResourceModel\Group\CollectionFactory $groupCollFactory
+        private \Magento\Customer\Model\ResourceModel\Group\CollectionFactory $groupCollFactory
     ) {
     }
 
     public function toOptionArray(): array
     {
         if ($this->options === null) {
-            /** @var \Magento\Store\Model\ResourceModel\Group\Collection $groups */
+            /** @var \Magento\Customer\Model\ResourceModel\Group\Collection $groups */
             $groups = $this->groupCollFactory->create();
-            $this->options = $groups->setLoadDefault(true)->load()->toOptionArray();
+            $this->options = $groups->load()->toOptionArray();
         }
         return $this->options;
     }
